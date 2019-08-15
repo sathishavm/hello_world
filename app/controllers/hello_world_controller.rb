d4b485b4428d80b5c836835af1e6121f33d06cd8
sathish <sathishavm@gmail.com>
2019-08-15 16:38:17 -0700

    change dep

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,7 +1,7 @@
 class HelloWorldController < ApplicationController
 
 	def index
-		@note = "Hello!! This is normal deployment"
+		@note = "Hello!! This is normal deployment."
 		@version ||= "test"
 		DDOG.emit_point("zendesk.spinnaker.hello_world.#{@version}", 1) if ENV['DDOG_API_KEY']
 	end
